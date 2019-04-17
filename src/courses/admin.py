542995f9239cce0ee821d4efd59bfa7e4a23527e
Christian <jcromerohdz@gmail.com>
2019-04-17 13:09:40 -0700

    Add lecture to the admin site and make a unique slug in lectures

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Course
-admin.site.register(Course)
+from .models import Course, Lecture
+
+class LectureInline(admin.TabularInline):
+    model = Lecture
+    prepopulated_fields = {"slug": ("title",)}
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LectureInline]
+    list_filter = ['updated', 'timestamp']
+    list_display = ['title', 'updated', 'timestamp']
+    readonly_fields = ['updated', 'timestamp', 'short_title']
+    search_field = ['title', 'description']
+
+    class Meta:
+        model = Course
+
+    def short_title(self, obj):
+        return obj.title[:3]
+
+admin.site.register(Course, CourseAdmin)
 
--- src/courses/models.py
+++ src/courses/models.py
@@ -4,10 +4,10 @@ from django.db import models
 from django.db.models.signals import pre_save
 from django.utils.text import slugify
 
+from videos.models import Video
 from .utils import create_slug
 
 class Course(models.Model):
-        
     user            = models.ForeignKey(settings.AUTH_USER_MODEL,  on_delete=models.CASCADE)
     title           = models.CharField(max_length=120)
     slug            = models.SlugField(blank=True)
@@ -23,6 +23,25 @@ class Course(models.Model):
         # return "/videos/{slug_arg}".format(slug_arg=self.slug)
         return reverse('courses:detail', kwargs={'slug':self.slug})
 
+class Lecture(models.Model):   
+    course          = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True)
+    video           = models.ForeignKey(Video, on_delete=models.SET_NULL, null=True)
+    title           = models.CharField(max_length=120)
+    slug            = models.SlugField(blank=True)
+    description     = models.TextField(blank=True)
+    updated         = models.DateTimeField(auto_now=True)
+    timestamp       = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        unique_together = (('slug', 'course'),)
+    
+    def get_absolute_url(self):
+        # return "/videos/{slug_arg}".format(slug_arg=self.slug)
+        return reverse('courses:detail', kwargs={'slug':course.slug})
+
 def pre_save_course_receiver(sender , instance, *args, **kwargs): 
     if not instance.slug:
         # instance.slug = slugify(instance.title) 
@@ -30,3 +49,4 @@ def pre_save_course_receiver(sender , instance, *args, **kwargs):
       
 
 pre_save.connect(pre_save_course_receiver, sender=Course)
+pre_save.connect(pre_save_course_receiver, sender=Lecture)
